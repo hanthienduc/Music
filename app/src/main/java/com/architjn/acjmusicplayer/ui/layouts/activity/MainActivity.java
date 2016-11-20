e4abb07ba142ecebf8c4ada614ff68f738323e47
Jack Boswell <boswelja@outlook.com>
2016-11-20 16:33:55 +1300

    Runtime permissions

--- app/src/main/java/com/architjn/acjmusicplayer/ui/layouts/activity/MainActivity.java
+++ app/src/main/java/com/architjn/acjmusicplayer/ui/layouts/activity/MainActivity.java
@@ -1,7 +1,9 @@
 package com.architjn.acjmusicplayer.ui.layouts.activity;
 
+import android.Manifest;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
@@ -44,13 +46,7 @@ public class MainActivity extends AppCompatActivity {
         overridePendingTransition(0, 0);
         Toolbar toolbar = (Toolbar) findViewById(R.id.main_toolbar);
         setSupportActionBar(toolbar);
-
-        init();
-        setDrawer();
-
-        Intent i = new Intent(MainActivity.this, MusicService.class);
-        startService(i);
-
+        fab = (FloatingActionButton) findViewById(R.id.fab_main);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -59,18 +55,13 @@ public class MainActivity extends AppCompatActivity {
                 sendBroadcast(requestSongDetials);
             }
         });
+        if(checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED || checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+        }
 
-        setupViewPager(viewPager);
-        TabLayout tabLayout = (TabLayout) findViewById(R.id.main_tablayout);
-        if (settingsPref.getBoolean("pref_extend_tabs", false))
-            tabLayout.setTabMode(TabLayout.MODE_FIXED);
-        else
-            tabLayout.setTabMode(TabLayout.MODE_SCROLLABLE);
-        tabLayout.setupWithViewPager(viewPager);
     }
 
     private void init() {
-        fab = (FloatingActionButton) findViewById(R.id.fab_main);
         viewPager = (ViewPager) findViewById(R.id.main_viewPager);
         toolbar = (Toolbar) findViewById(R.id.main_toolbar);
         settingsPref = PreferenceManager.getDefaultSharedPreferences(getBaseContext());
@@ -171,6 +162,30 @@ public class MainActivity extends AppCompatActivity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch(requestCode) {
+            case 1:
+                if(grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    init();
+                    setDrawer();
+
+                    Intent i = new Intent(MainActivity.this, MusicService.class);
+                    startService(i);
+
+                    setupViewPager(viewPager);
+                    TabLayout tabLayout = (TabLayout) findViewById(R.id.main_tablayout);
+                    if (settingsPref.getBoolean("pref_extend_tabs", false))
+                        tabLayout.setTabMode(TabLayout.MODE_FIXED);
+                    else
+                        tabLayout.setTabMode(TabLayout.MODE_SCROLLABLE);
+                    tabLayout.setupWithViewPager(viewPager);
+                } else {
+                    finish();
+                }
+        }
     }
 
 }
