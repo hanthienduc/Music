406d8d682160121bfccd1721ba695e5d75f0d5e5
Jack Boswell <boswelja@outlook.com>
2016-12-20 23:37:18 +1300

    Improve album grid view * Also try to fix audio focus requests

--- app/src/main/java/com/dominionos/music/task/ColorGridTask.java
+++ app/src/main/java/com/dominionos/music/task/ColorGridTask.java
@@ -3,22 +3,20 @@ package com.dominionos.music.task;
 import android.animation.ArgbEvaluator;
 import android.animation.ValueAnimator;
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.graphics.Color;
 import android.os.AsyncTask;
 import android.support.v4.content.res.ResourcesCompat;
 import android.support.v7.graphics.Palette;
-import android.util.Log;
 
 import com.dominionos.music.R;
 import com.dominionos.music.utils.adapters.AlbumsAdapter;
 
-public class ColorGridTask extends AsyncTask<Void, Void, Void> {
+public class ColorGridTask extends AsyncTask<Object, Object, Void> {
 
     private final Context context;
     private final String artPath;
     private final AlbumsAdapter.SimpleItemViewHolder holder;
-    private ValueAnimator colorAnimation;
 
     public ColorGridTask(Context context, String artPath, AlbumsAdapter.SimpleItemViewHolder holder) {
         this.context = context;
@@ -28,53 +26,55 @@ public class ColorGridTask extends AsyncTask<Void, Void, Void> {
     }
 
     @Override
-    protected Void doInBackground(Void... params) {
-        Log.v("artPath", artPath);
-        Palette.generateAsync(BitmapFactory.decodeFile(artPath),
-                new Palette.PaletteAsyncListener() {
-                    @Override
-                    public void onGenerated(final Palette palette) {
-                        Integer colorFrom = ResourcesCompat.getColor(context.getResources(), R.color.card_background, null);
-                        Integer colorTo = palette.getVibrantColor(ResourcesCompat.getColor(context.getResources(), R.color.card_background, null));
-                        colorAnimation = ValueAnimator.ofObject(new ArgbEvaluator(), colorFrom, colorTo);
-                        colorAnimation.setDuration(1000);
-                        colorAnimation.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+    protected Void doInBackground(Object... params) {
+        Palette.PaletteAsyncListener paletteAsyncListener = new Palette.PaletteAsyncListener() {
+            @Override
+            public void onGenerated(Palette palette) {
+                if (palette.getVibrantSwatch() != null) {
+                    Palette.Swatch vibrantSwatch = palette.getVibrantSwatch();
+                    int colorFrom = ResourcesCompat.getColor(context.getResources(), R.color.card_background, null);
+                    int colorTo = vibrantSwatch.getRgb();
+                    ValueAnimator colorAnimation = ValueAnimator.ofObject(new ArgbEvaluator(), colorFrom, colorTo);
+                    int duration = 800;
+                    colorAnimation.setDuration(duration);
+                    colorAnimation.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
 
-                            @Override
-                            public void onAnimationUpdate(ValueAnimator animator) {
-                                holder.textHolder.setBackgroundColor((Integer) animator.getAnimatedValue());
-                            }
-
-                        });
-                        colorAnimation.start();
-                        try {
-                            Integer colorFrom1 = Color.parseColor("#ffffff");
-                            Integer colorTo1 = palette.getVibrantSwatch().getTitleTextColor();
-                            colorAnimation = ValueAnimator.ofObject(new ArgbEvaluator(), colorFrom1, colorTo1);
-                            colorAnimation.setDuration(800);
-                            colorAnimation.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
-                                @Override
-                                public void onAnimationUpdate(ValueAnimator animator) {
-                                    holder.albumName.setTextColor((Integer) animator.getAnimatedValue());
-                                }
-                            });
-                            colorAnimation.start();
-                            Integer colorFrom2 = Color.parseColor("#adb2bb");
-                            Integer colorTo2 = palette.getVibrantSwatch().getTitleTextColor();
-                            colorAnimation = ValueAnimator.ofObject(new ArgbEvaluator(), colorFrom2, colorTo2);
-                            colorAnimation.setDuration(800);
-                            colorAnimation.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
-                                @Override
-                                public void onAnimationUpdate(ValueAnimator animator) {
-                                    holder.albumDesc.setTextColor((Integer) animator.getAnimatedValue());
-                                }
-                            });
-                            colorAnimation.start();
-                        } catch (NullPointerException e) {
-                            e.printStackTrace();
+                        @Override
+                        public void onAnimationUpdate(ValueAnimator animator) {
+                            holder.textHolder.setBackgroundColor((Integer) animator.getAnimatedValue());
                         }
-                    }
-                });
+
+                    });
+                    colorAnimation.start();
+                    colorFrom = ResourcesCompat.getColor(context.getResources(), android.R.color.primary_text_dark, null);
+                    colorTo = vibrantSwatch.getBodyTextColor();
+                    colorAnimation = ValueAnimator.ofObject(new ArgbEvaluator(), colorFrom, colorTo);
+                    colorAnimation.setDuration(duration);
+                    colorAnimation.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+                        @Override
+                        public void onAnimationUpdate(ValueAnimator animator) {
+                            holder.albumName.setTextColor((Integer) animator.getAnimatedValue());
+                        }
+                    });
+                    colorAnimation.start();
+                    colorFrom = ResourcesCompat.getColor(context.getResources(), android.R.color.secondary_text_dark, null);
+                    colorTo = vibrantSwatch.getTitleTextColor();
+                    colorAnimation = ValueAnimator.ofObject(new ArgbEvaluator(), colorFrom, colorTo);
+                    colorAnimation.setDuration(duration);
+                    colorAnimation.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+                        @Override
+                        public void onAnimationUpdate(ValueAnimator animator) {
+                            holder.albumDesc.setTextColor((Integer) animator.getAnimatedValue());
+                        }
+                    });
+                    colorAnimation.start();
+                }
+            }
+        };
+        Bitmap image = BitmapFactory.decodeFile(artPath);
+        if(image != null) {
+            Palette.from(image).generate(paletteAsyncListener);
+        }
         return null;
     }
 
