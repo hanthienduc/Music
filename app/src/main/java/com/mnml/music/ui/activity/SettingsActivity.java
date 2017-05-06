355350e84b26dc41b17699c5de75f8872c521e3f
Jack Boswell (boswelja) <boswelja@outlook.com>
2017-05-06 18:55:04 +1200

    Rewrite most of settings * 'Follow substratum theme' is now 'Reset theme colors' or 'Use substratum theme colors' if subs is detected

--- app/src/main/java/com/mnml/music/ui/activity/SettingsActivity.java
+++ app/src/main/java/com/mnml/music/ui/activity/SettingsActivity.java
@@ -1,5 +1,6 @@
 package com.mnml.music.ui.activity;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.FragmentManager;
@@ -8,11 +9,13 @@ import android.support.v7.preference.Preference;
 import android.support.v7.preference.SeekBarPreference;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import com.afollestad.materialdialogs.MaterialDialog;
 import com.afollestad.materialdialogs.color.ColorChooserDialog;
 import com.kabouzeid.appthemehelper.common.prefs.supportv7.ATESwitchPreference;
 import com.kabouzeid.appthemehelper.common.prefs.supportv7.ATEColorPreference;
 import com.kabouzeid.appthemehelper.common.prefs.supportv7.ATEPreferenceFragmentCompat;
 import com.mnml.music.R;
+import com.mnml.music.utils.Config;
 import com.mnml.music.utils.Utils;
 import com.kabouzeid.appthemehelper.ATH;
 import com.kabouzeid.appthemehelper.ThemeStore;
@@ -69,9 +72,19 @@ public class SettingsActivity extends ATHToolbarActivity
     }
 
 
-    public static class SettingsFragment extends ATEPreferenceFragmentCompat {
+    public static class SettingsFragment extends ATEPreferenceFragmentCompat implements Preference.OnPreferenceChangeListener, Preference.OnPreferenceClickListener {
+        private static final String KEY_DARK_MODE = "dark_theme";
+        private static final String KEY_RESET_THEME = "reset_theme";
+        private static final String KEY_COLOR_PRIMARY = "primary_color";
+        private static final String KEY_COLOR_ACCENT = "accent_color";
+        private static final String KEY_PLAYBACK_SPEED = "playback_speed";
+        private static final String KEY_GOOGLE_SERVICES = "google_services";
+
+        private Context context;
+
         @Override
         public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+            context = getContext();
             addPreferencesFromResource(R.xml.prefs_appearance);
             configureAppearanceSettings();
 
@@ -83,93 +96,99 @@ public class SettingsActivity extends ATHToolbarActivity
         }
 
         private void configureGeneralSettings() {
-            final ATESwitchPreference googleServices = (ATESwitchPreference) findPreference("google_services");
-            final boolean isGoogleServicesAvailable = Utils.isGooglePlayServicesAvailable(getContext());
+            final ATESwitchPreference googleServices = (ATESwitchPreference) findPreference(KEY_GOOGLE_SERVICES);
+            final boolean isGoogleServicesAvailable = Utils.isGooglePlayServicesAvailable(context);
             if(!isGoogleServicesAvailable) {
-                googleServices.getSharedPreferences().edit().putBoolean("google_services", false).apply();
+                googleServices.getSharedPreferences().edit().putBoolean(KEY_GOOGLE_SERVICES, false).apply();
             }
             googleServices.setEnabled(isGoogleServicesAvailable);
         }
 
         private void configurePlaybackSettings() {
-            final SeekBarPreference playbackSpeed = (SeekBarPreference) findPreference("playback_speed");
-            playbackSpeed.setSummary((playbackSpeed.getValue() / 10.0f) + "x");
-            playbackSpeed.setOnPreferenceChangeListener(
-                    (preference, newValue) -> {
-                        float value = ((int) newValue) / 10.0f;
-                        preference.getSharedPreferences().edit().putFloat("playback_speed_float", value).apply();
-                        preference.setSummary(value + "x");
-                        return true;
-                    });
+            final SeekBarPreference playbackSpeed = (SeekBarPreference) findPreference(KEY_PLAYBACK_SPEED);
+            playbackSpeed.setOnPreferenceChangeListener(this);
+            playbackSpeed.callChangeListener(playbackSpeed.getValue());
         }
 
         private void configureAppearanceSettings() {
-            Preference substratumTheme = findPreference("substratum_theme");
+            final Preference darkMode = findPreference(KEY_DARK_MODE);
+            darkMode.setOnPreferenceChangeListener(this);
+            darkMode.callChangeListener(darkMode.getSharedPreferences().getBoolean(KEY_DARK_MODE, false));
 
-            final String darkModeEnabled = getString(R.string.dark_mode_enabled);
-            final String darkModeDisabled = getString(R.string.dark_mode_disabled);
-            Preference darkMode = findPreference("dark_theme");
-            darkMode.setSummary(
-                    darkMode.getSharedPreferences().getBoolean("dark_theme", false)
-                            ? darkModeEnabled
-                            : darkModeDisabled);
-            darkMode.setOnPreferenceChangeListener(
-                    (preference, newValue) -> {
-                        preference.setSummary(newValue.equals(true) ? darkModeEnabled : darkModeDisabled);
+            final ATEColorPreference colorPrimaryPref = (ATEColorPreference) findPreference(KEY_COLOR_PRIMARY);
+            colorPrimaryPref.setColor(ThemeStore.primaryColor(getActivity()), ThemeStore.primaryColor(getActivity()));
+            colorPrimaryPref.setOnPreferenceClickListener(this);
+
+            final ATEColorPreference colorAccentPref = (ATEColorPreference) findPreference(KEY_COLOR_ACCENT);
+            colorAccentPref.setColor(ThemeStore.accentColor(getActivity()), ThemeStore.accentColor(getActivity()));
+            colorAccentPref.setOnPreferenceClickListener(this);
+
+            final Preference resetTheme = findPreference(KEY_RESET_THEME);
+            boolean isSubstratumInstalled = Utils.isSubsInstalled(context);
+            resetTheme.setTitle(isSubstratumInstalled ? getString(R.string.substratum_theme) : getString(R.string.reset_theme));
+            resetTheme.setOnPreferenceClickListener(this);
+        }
+
+        @Override
+        public boolean onPreferenceChange(Preference preference, Object newValue) {
+            switch(preference.getKey()) {
+                case KEY_DARK_MODE:
+                    preference.setSummary(newValue.equals(true) ? getString(R.string.dark_mode_enabled) : getString(R.string.dark_mode_disabled));
+                    if(!newValue.equals(preference.getSharedPreferences().getBoolean(KEY_DARK_MODE, false))) {
                         ThemeStore.editTheme(getActivity())
                                 .activityTheme(((Boolean) newValue) ? R.style.AppTheme_Dark : R.style.AppTheme_Light)
                                 .commit();
                         getActivity().recreate();
-                        return true;
-                    });
+                    }
+                    return true;
+                case KEY_PLAYBACK_SPEED:
+                    float value = ((int) newValue) / 10.0f;
+                    preference.getSharedPreferences().edit().putFloat("playback_speed_float", value).apply();
+                    preference.setSummary(value + "x");
+                    return true;
+            }
+            return false;
+        }
 
-            boolean subsTheme =
-                    substratumTheme.getSharedPreferences().getBoolean("substratum_theme", false);
-            ATEColorPreference colorPrimaryPref = (ATEColorPreference) findPreference("primary_color");
-            colorPrimaryPref.setEnabled(!subsTheme);
-            colorPrimaryPref.setColor(
-                    ThemeStore.primaryColor(getActivity()), ThemeStore.primaryColor(getActivity()));
-            colorPrimaryPref.setOnPreferenceClickListener(
-                    preference -> {
-                        new ColorChooserDialog.Builder((SettingsActivity) getActivity(), R.string.primary_color)
-                                .preselect(ThemeStore.primaryColor(getActivity()))
-                                .accentMode(false)
-                                .allowUserColorInput(true)
-                                .allowUserColorInputAlpha(false)
-                                .show();
-                        return true;
-                    });
-
-            ATEColorPreference colorAccentPref = (ATEColorPreference) findPreference("accent_color");
-            colorAccentPref.setEnabled(!subsTheme);
-            colorAccentPref.setColor(
-                    ThemeStore.accentColor(getActivity()), ThemeStore.accentColor(getActivity()));
-            colorAccentPref.setOnPreferenceClickListener(
-                    preference -> {
-                        new ColorChooserDialog.Builder((SettingsActivity) getActivity(), R.string.accent_color)
-                                .preselect(ThemeStore.accentColor(getActivity()))
-                                .accentMode(true)
-                                .allowUserColorInput(true)
-                                .allowUserColorInputAlpha(false)
-                                .show();
-                        return true;
-                    });
-
-            boolean isSubstratumInstalled = Utils.isSubsInstalled(getContext());
-            substratumTheme.setEnabled(isSubstratumInstalled);
-            substratumTheme.setOnPreferenceChangeListener(
-                    (preference, newValue) -> {
-                        colorPrimaryPref.setEnabled(!(boolean) newValue);
-                        colorAccentPref.setEnabled(!(boolean) newValue);
-                        if ((boolean) newValue) {
-                            ThemeStore.editTheme(getContext())
-                                    .primaryColorRes(R.color.colorPrimary)
-                                    .accentColorRes(R.color.colorAccent)
-                                    .commit();
-                            getActivity().recreate();
-                        }
-                        return true;
-                    });
+        @Override
+        public boolean onPreferenceClick(Preference preference) {
+            switch(preference.getKey()) {
+                case KEY_COLOR_PRIMARY:
+                    new ColorChooserDialog.Builder((SettingsActivity) getActivity(), R.string.primary_color)
+                            .preselect(ThemeStore.primaryColor(getActivity()))
+                            .accentMode(false)
+                            .allowUserColorInput(true)
+                            .dynamicButtonColor(false)
+                            .allowUserColorInputAlpha(false)
+                            .show();
+                    return true;
+                case KEY_COLOR_ACCENT:
+                    new ColorChooserDialog.Builder((SettingsActivity) getActivity(), R.string.accent_color)
+                            .preselect(ThemeStore.accentColor(getActivity()))
+                            .accentMode(true)
+                            .allowUserColorInput(true)
+                            .customColors(Config.ACCENT_COLORS, Config.ACCENT_COLORS_SUB)
+                            .dynamicButtonColor(false)
+                            .allowUserColorInputAlpha(false)
+                            .show();
+                    return true;
+                case KEY_RESET_THEME:
+                    new MaterialDialog.Builder(context)
+                            .title(preference.getTitle())
+                            .content(getString(R.string.reset_theme_confirmation))
+                            .positiveText(getString(R.string.yes))
+                            .negativeText(getString(R.string.cancel))
+                            .onPositive((materialDialog, dialogAction) -> {
+                                ThemeStore.editTheme(context)
+                                        .primaryColor(context.getColor(R.color.colorPrimary))
+                                        .accentColor(context.getColor(R.color.colorAccent))
+                                        .commit();
+                                getActivity().recreate();
+                            })
+                            .show();
+                    return true;
+            }
+            return false;
         }
     }
 
--- app/src/main/java/com/mnml/music/utils/Config.java
+++ app/src/main/java/com/mnml/music/utils/Config.java
@@ -1,5 +1,7 @@
 package com.mnml.music.utils;
 
+import android.graphics.Color;
+
 public class Config {
     public static final int DONATE_REQUEST_CODE = 5963;
 
@@ -11,6 +13,7 @@ public class Config {
     // Button state alpha values
     public static final float BUTTON_ACTIVE_ALPHA = 1.0f;
     public static final float BUTTON_INACTIVE_ALPHA = 0.5f;
+
     public static final int ALBUM_CARD_WIDTH = 180;
 
     // Intent values
@@ -40,11 +43,134 @@ public class Config {
 
     // Shortcuts
     public static final String KEY_SHORTCUT_TYPE = "ShortcutType";
-
     public static final int SHORTCUT_TYPE_NONE = 0;
     public static final int SHORTCUT_TYPE_SHUFFLE_ALL = 1;
     public static final int SHORTCUT_TYPE_PLAY_ALL = 2;
 
     static final int ANIMATION_DURATION = 500;
     public static final int NOTIFICATION_ID = 596;
+
+    public final static int[] ACCENT_COLORS = new int[]{
+            Color.parseColor("#FF1744"),
+            Color.parseColor("#F50057"),
+            Color.parseColor("#D500F9"),
+            Color.parseColor("#651FFF"),
+            Color.parseColor("#3D5AFE"),
+            Color.parseColor("#2979FF"),
+            Color.parseColor("#00B0FF"),
+            Color.parseColor("#00E5FF"),
+            Color.parseColor("#1DE9B6"),
+            Color.parseColor("#00E676"),
+            Color.parseColor("#76FF03"),
+            Color.parseColor("#C6FF00"),
+            Color.parseColor("#FFEA00"),
+            Color.parseColor("#FFC400"),
+            Color.parseColor("#FF9100"),
+            Color.parseColor("#FF3D00"),
+            Color.parseColor("#000000")
+    };
+
+    // Custom colors, including the entire material palette
+    public final static int[][] ACCENT_COLORS_SUB = new int[][]{
+            new int[]{
+                    Color.parseColor("#FF8A80"),
+                    Color.parseColor("#FF5252"),
+                    Color.parseColor("#FF1744"),
+                    Color.parseColor("#D50000")
+            },
+            new int[]{
+                    Color.parseColor("#FF80AB"),
+                    Color.parseColor("#FF4081"),
+                    Color.parseColor("#F50057"),
+                    Color.parseColor("#C51162")
+            },
+            new int[]{
+                    Color.parseColor("#EA80FC"),
+                    Color.parseColor("#E040FB"),
+                    Color.parseColor("#D500F9"),
+                    Color.parseColor("#AA00FF")
+            },
+            new int[]{
+                    Color.parseColor("#B388FF"),
+                    Color.parseColor("#7C4DFF"),
+                    Color.parseColor("#651FFF"),
+                    Color.parseColor("#6200EA")
+            },
+            new int[]{
+                    Color.parseColor("#8C9EFF"),
+                    Color.parseColor("#536DFE"),
+                    Color.parseColor("#3D5AFE"),
+                    Color.parseColor("#304FFE")
+            },
+            new int[]{
+                    Color.parseColor("#82B1FF"),
+                    Color.parseColor("#448AFF"),
+                    Color.parseColor("#2979FF"),
+                    Color.parseColor("#2962FF")
+            },
+            new int[]{
+                    Color.parseColor("#80D8FF"),
+                    Color.parseColor("#40C4FF"),
+                    Color.parseColor("#00B0FF"),
+                    Color.parseColor("#0091EA")
+            },
+            new int[]{
+                    Color.parseColor("#84FFFF"),
+                    Color.parseColor("#18FFFF"),
+                    Color.parseColor("#00E5FF"),
+                    Color.parseColor("#00B8D4")
+            },
+            new int[]{
+                    Color.parseColor("#A7FFEB"),
+                    Color.parseColor("#64FFDA"),
+                    Color.parseColor("#1DE9B6"),
+                    Color.parseColor("#00BFA5")
+            },
+            new int[]{
+                    Color.parseColor("#B9F6CA"),
+                    Color.parseColor("#69F0AE"),
+                    Color.parseColor("#00E676"),
+                    Color.parseColor("#00C853")
+            },
+            new int[]{
+                    Color.parseColor("#CCFF90"),
+                    Color.parseColor("#B2FF59"),
+                    Color.parseColor("#76FF03"),
+                    Color.parseColor("#64DD17")
+            },
+            new int[]{
+                    Color.parseColor("#F4FF81"),
+                    Color.parseColor("#EEFF41"),
+                    Color.parseColor("#C6FF00"),
+                    Color.parseColor("#AEEA00")
+            },
+            new int[]{
+                    Color.parseColor("#FFFF8D"),
+                    Color.parseColor("#FFFF00"),
+                    Color.parseColor("#FFEA00"),
+                    Color.parseColor("#FFD600")
+            },
+            new int[]{
+                    Color.parseColor("#FFE57F"),
+                    Color.parseColor("#FFD740"),
+                    Color.parseColor("#FFC400"),
+                    Color.parseColor("#FFAB00")
+            },
+            new int[]{
+                    Color.parseColor("#FFD180"),
+                    Color.parseColor("#FFAB40"),
+                    Color.parseColor("#FF9100"),
+                    Color.parseColor("#FF6D00")
+            },
+            new int[]{
+                    Color.parseColor("#FF9E80"),
+                    Color.parseColor("#FF6E40"),
+                    Color.parseColor("#FF3D00"),
+                    Color.parseColor("#DD2C00")
+            },
+            new int[]{
+                    Color.parseColor("#000000"),
+                    Color.parseColor("#FFFFFF")
+            }
+    };
 }
